9b1694834d324c281376193c4df2c47b604c1b16
Lukas Braun <mail@lukasbraun.com>
2020-04-05 19:04:59 +0100

    Resolve async errors

--- view/js/bookmarks.js
+++ view/js/bookmarks.js
@@ -2,18 +2,67 @@
 * Handle bookmark functionality
 */
 
+
+function Bookmarks() {
+  var that = this;
+  
+  this.test = function(tree) {
+    return tree;
+  };
+  
+  this.retrieve_meta = function(items) {
+    items.forEach(function(item) {
+      fetch(item.url).then((response) => {
+        item.status = response.status;
+        item.contentType = response.headers.get("Content-Type");
+        item.category = (item.status == 200) ? "default" : "broken";
+        item.info = {};
+      })
+    });
+    return items;
+  };
+  
+  this.retrieve_items = function(result, item) {
+    if(item.type === "bookmark") {
+      return [item];
+    }
+    
+    if(item.children) {
+      item.children.forEach(function(child) {
+        result = result.concat(that.retrieve_items([], child));
+      });
+      return result;
+    }
+  };
+  
+  this.load_bookmarks = function() {
+    return browser.bookmarks.getTree();
+  };
+}
+
+
+/*
+
 function Tree(data) {
   var that = this;
   this.data = data;
   
-  this.retrieve_details = function(items) {
+  this.retrieve_meta = function(items) {
+    let results = [];
     items.forEach(function(item) {
-      fetch(item.url, {
-        mode: 'cors',
-        headers: {
-          'Access-Control-Allow-Origin':'*'
-        }
-      }).then((response) => {
+      fetch(item.url).then((response) => {
+        item.status = response.status;
+        item.contentType = response.headers.get("Content-Type");
+        item.category = (item.status == 200) ? "default" : "broken";
+        item.info = {};
+      })
+    });
+    return items;
+  };
+  
+  this.retrieve_info = function(items) {
+    items.forEach(function(item) {
+      fetch(item.url).then((response) => {
         item.status = response.status;
         item.contentType = response.headers.get("Content-Type");
         item.category = "default";
@@ -58,9 +107,9 @@ function Tree(data) {
     }
   };
   
-  this.items = this.extract_items([], data);
-  
-  
+  this.items = (async function() {return await that.retrieve_meta(that.extract_items([], data))}()).value;
+  console.log(items);
+*/
   
   /*
   this.iterate = function(item, snippet, depth) {
@@ -80,18 +129,46 @@ function Tree(data) {
   }
   */
   
+/*
   this.render = function(snippet) {
     this.iterate(this.data, snippet, 0)
   }
   
   this.getLatest = function(start, end) {
+    console.log(that.items);
     that.items.sort(function(a, b) {
       return b.dateAdded - a.dateAdded;
     });
-    return {index: 0, title: "Newest", children: that.retrieve_details(that.items.slice(start, end))};
+    return {index: 0, title: "Newest", children: that.retrieve_info(that.items.slice(start, end))};
   };
 }
 
+
+this.extract_items = function(result, item) {
+    if(item.type === "bookmark") {
+      return [item];
+    }
+    
+    if(item.children) {
+      item.children.forEach(function(child) {
+        result = result.concat(that.extract_items([], child));
+      });
+      return result;
+    }
+  };
+
+
+function extract_items
+
+
+function retrieve_meta(bookmarksTree) {
+  
+};
+
+
+function load_bookmarks() {
+  return browser.bookmarks.getTree();
+};
 
 function Bookmarks() {
   var that = this;
@@ -102,19 +179,63 @@ function Bookmarks() {
       console.log("Error:", error);
     });
   }();
+  console.log(this.fullTree);
   
   this.getTree = function() {
     return this.fullTree;
   };
-  
-  /*
   browser.bookmarks.getRecent(1).then((bookmarks) => {
     this.listBookmarks(bookmarks);
   })
   .catch((error) => {
     console.log("Error:", error);
   });
-  */
 }
 
+
+  */
+
+/*
+
+  
+  this.retrieve_meta3 = async function(items) {
+    for(var i=0; i<items.length; i++) {
+      try {
+        let item = await fetch(items[i].url).then((response) => {
+          items[i].status = response.status;
+          return items[i];
+        });
+        items[i] = item;
+        return items;
+      } catch(e) {
+        console.log(e);
+      };
+    }
+  };
+  
+  this.retrieve_meta2 = function(items) {
+    var items_ = function() {
+      result = [];
+      items.forEach(function(item) {
+        result.push(fetch(item.url).then((response) => {
+          item.status = response.status;
+          item.contentType = response.headers.get("Content-Type");
+          item.category = (item.status == 200) ? "default" : "broken";
+          item.info = {};
+          
+          pageFilters.forEach(function(pageFilter) {
+            if(pageFilter.filter.test(item.url) && item.contentType.includes(pageFilter.expecteContentType)) {
+              item.category = pageFilter.category;
+            }
+          });
+          return item;
+        }));
+      });
+      console.log(result);
+      return result;
+    }();
+    console.log(items_);
+  };
+  
+*/
 
